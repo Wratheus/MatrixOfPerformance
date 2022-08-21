75bb739be7d45f27bd2b560be0776396bfdf5f3e
Aleksandr P <71131015+Wratheus@users.noreply.github.com>
2022-08-21 11:18:02 +0300

    Untrack files in .gitignore

--- flutter_matrix_of_skills/lib/src/core/database/client_credentials/auth.dart
+++
@@ -1,4 +0,0 @@
-class ClientCredentials {
-  final String key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1aWtmanF2cG14ZnV2bndpdnRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAwNjM5NzYsImV4cCI6MTk3NTYzOTk3Nn0.2fIme_j98Lg_WeZxSvjixI8mMY4_qg5XQOcOqzeLuR4';
-  final String url = 'https://juikfjqvpmxfuvnwivtb.supabase.co';
-}
